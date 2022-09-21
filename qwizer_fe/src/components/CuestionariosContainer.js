1ab3de06f56301d03c166cf702c2d4118092cbb1
iQuickJP <jbarthap@gmail.com>
2022-09-21 22:54:11 +0200

    Cambio de clases a funciones
    CuestionariosContainer y UploadQuestions actualizados a funciones

--- qwizer_fe/src/components/CuestionariosContainer.js
+++ qwizer_fe/src/components/CuestionariosContainer.js
@@ -1,41 +1,35 @@
 import React from 'react'
 import TarjetaCuestionario from './TarjetaCuestionario';
 
-class CuestionariosContainer extends React.Component {
+const CuestionariosContainer = (props) => {
+    const idCuestionarios = props.idCuestionarios;
+    const empezarTest = props.empezarTest;
+    const asignatura = props.asignatura;
+    const revisarTest = props.revisionTest;
+    const rol = props.rol;
+    const revisarNotasTest = props.revisarNotasTest;
+    const revisionTestProfesor = props.revisionTestProfesor;
 
-    constructor(props) {
-        super(props);
+    if (empezarTest) {
+        return (
+            <div className="index-body row">
+
+                <div className='section-title'><h1>{asignatura}</h1></div>
+                {props.cuestionarios.map((cuestionario, indx) => {
+                    return (
+                        <div className='d-flex justify-content-center'>
+                            <TarjetaCuestionario offline={false} cuestionario={cuestionario} idCuestionario={idCuestionarios[indx]} empezarTest={empezarTest} revisionTest={revisarTest} rol={rol} revisarNotasTest={revisarNotasTest} revisionTestProfesor={revisionTestProfesor}/>
+                        </div>
+                    )
+                })
+                }
+
+            </div>
+        );
+    } else {
+        return <h1>Loading...</h1>
     }
 
-    render() {
-        const idCuestionarios = this.props.idCuestionarios;
-        const empezarTest = this.props.empezarTest;
-        const asignatura = this.props.asignatura;
-        const revisarTest = this.props.revisionTest;
-        const rol = this.props.rol;
-        const revisarNotasTest = this.props.revisarNotasTest;
-        const revisionTestProfesor = this.props.revisionTestProfesor;
-        
-        if(empezarTest){
-            return(
-                <div className="index-body row">
-                    
-                        <div className='section-title'><h1>{asignatura}</h1></div>  
-                        { this.props.cuestionarios.map(function(cuestionario,indx){
-                            return (
-                                <div className='d-flex justify-content-center'>
-                                    <TarjetaCuestionario offline={false} cuestionario={cuestionario} idCuestionario={idCuestionarios[indx]} empezarTest={empezarTest} revisionTest={revisarTest} rol={rol} revisarNotasTest={revisarNotasTest} revisionTestProfesor={revisionTestProfesor}></TarjetaCuestionario>
-                                </div>
-                            )
-                        })
-                        }
-                                       
-                </div>
-             );
-        }else{
-            return <h1>Loading...</h1>
-        }
-      }
 }
 
 export default CuestionariosContainer;
--- qwizer_fe/src/components/UploadQuestions.js
+++ qwizer_fe/src/components/UploadQuestions.js
@@ -1,130 +1,104 @@
-import React, { Component } from 'react'
-import $ from 'jquery'; 
+import React, { useEffect, useState } from 'react'
 import ErrorModal from './common/modals/ErrorModal';
 import SuccessModal from './common/modals/SuccessModal';
-import {getSubjects} from '../utils/manage_subjects.js'
+import { getSubjects } from '../utils/manage_subjects.js'
 import { API_URL } from '../constants/Constants';
 
 
-export default class UploadQuestions extends Component {
+const UploadQuestions = (props) => {
+    const [file, setFile] = useState("")
+    const [message, setMessage] = useState(undefined)
+    const [asignaturasImpartidas, setAsignaturas] = useState([])
+    const [idAsignatura, setIdAsignatura] = useState("")
 
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         file:'',
-         message: undefined
-      };
-
-      this.setFile = this.setFile.bind(this);
-      this.uploadFile = this.uploadFile.bind(this);
-      this.getImparteAsignaturas = this.getImparteAsignaturas.bind(this);
-      
-    }
-
-    componentDidMount(){
-        this.getImparteAsignaturas();
-    }
-
-    getImparteAsignaturas = () => {
+    useEffect(() => {
         getSubjects().then(data => {
-            this.setState({
-              asignaturasImpartidas: data.asignaturas,
-            }); 
-          })
-    }
+            setAsignaturas(data.asignaturas);
+        })
+    }, [])
 
-    setFile = (e) =>{
-        this.setState({file: e.target.files[0]})
-        e.target.value = ''
-    }
-
-    uploadFile = () => {
-        if(this.state.file.name !== "" && this.state.idAsignatura){
+    const uploadFile = () => {
+        if (file.name !== "" && idAsignatura) {
             let reader = new FileReader();
-            reader.readAsText(this.state.file,'utf-8');
-            reader.onload = (e) =>{               
-                const fichero_yaml = new Map([["fichero_yaml", e.target.result],["idAsignatura",this.state.idAsignatura]]);
-                const jsonObject = JSON.stringify(Object.fromEntries(fichero_yaml));
-                var token = localStorage.getItem('token');
+            reader.readAsText(file, 'utf-8');
+            reader.onload = (e) => {
+                const fichero_yaml = {fichero: e.target.result, idA: idAsignatura};
+                const jsonObject = JSON.stringify(fichero_yaml);
+                let token = localStorage.getItem('token');
 
                 fetch(`${API_URL}/upload-questions`, {
-                method: 'POST',
-                headers:{
-                    'Content-type': 'application/json',
-                    'Authorization': token
-                },
-                body: jsonObject
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json',
+                        'Authorization': token
+                    },
+                    body: jsonObject
                 })
-                .then(response => response.json())
-                .then(data => {
-                
-                    this.setState({
-                        file: "",
-                        message: data.message
-                    });
-                    if(data.inserted === "false"){
-                        window.$("#inserted_error").modal('show');
-                    } 
-                    else{
-                        window.$("#inserted_success").modal('show');
-                    } 
-                             
+                    .then(response => response.json())
+                    .then(data => {
+
+                        setFile("")
+                        setMessage(data.message)
+                        if (data.inserted === "false") {
+                            window.$("#inserted_error").modal('show');
+                        }
+                        else {
+                            window.$("#inserted_success").modal('show');
+                        }
+
                     })
-                .catch(error => console.log(error));
+                    .catch(error => console.log(error));
             }
         }
     }
 
-    seleccionaAsignatura = () => {
+    const seleccionaAsignatura = () => {
         return <div className=''>
-                    
-                    <select className='form-select margin-top-upload-question' defaultValue='null' onChange={(e) => this.setState({idAsignatura:Number(e.target.value)})}>
-                                {this.state.asignaturasImpartidas.map((subject,indx) => {
-                                                return (
-                                                    <option key={indx} value={subject.id}>{subject.nombre}</option>
-                                                );
-                                })}
-                                <option key='null' value='null' hidden> Selecciona una Asignatura </option>
-                                </select>
-                    
-                </div>
+
+            <select className='form-select margin-top-upload-question' defaultValue='null' onChange={(e) => setIdAsignatura(Number(e.target.value))}>
+                {asignaturasImpartidas.map((subject, indx) => {
+                    return (
+                        <option key={indx} value={subject.id}>{subject.nombre}</option>
+                    );
+                })}
+                <option key='null' value='null' hidden> Selecciona una Asignatura </option>
+            </select>
+
+        </div>
     }
 
-    render() {
-
-        if(this.state.asignaturasImpartidas){
-            return (
-                <div className="upload-body">
-                    <div className="card upload-section ">
-                        <div className="header bg-blue-grey">
-                            <h2>Sube tus preguntas en formato : YAML</h2>
-                        </div>
-                        <div className='upload-inner-body'>
-                            <h4><label htmlFor="myfile">Selecciona un archivo:</label></h4>
-                            <div className="input-group">
-                                <div className="custom-file">
-                                    <input type="file" className="custom-file-input" aria-describedby="inputGroupFileAddon01" onChange={(e) => this.setFile(e)} id="myfile"  name="myfile"/>
-                                     <label className="custom-file-label" htmlFor="inputGroupFile01">{this.state.file.name}</label>
-                                </div>
-                            </div>
-                            
-                            {this.seleccionaAsignatura()}
-                            
-                            <div className="upload-message-section">
-                            {this.state.file !== '' && this.state.idAsignatura && 
-                                <button type="button" className="btn btn-success btn-submit" onClick={this.uploadFile}>Subir Preguntas</button>
-                            }
-                            </div>
-                        </div>   
-
+    if (asignaturasImpartidas) {
+        return (
+            <div className="upload-body">
+                <div className="card upload-section ">
+                    <div className="header bg-blue-grey">
+                        <h2>Sube tus preguntas en formato : YAML</h2>
                     </div>
-                    <ErrorModal id={"inserted_error"} message={this.state.message}></ErrorModal>
-                    <SuccessModal id={"inserted_success"} message={this.state.message}></SuccessModal>
-                </div>
-            )
-        }
+                    <div className='upload-inner-body'>
+                        <h4><label htmlFor="myfile">Selecciona un archivo:</label></h4>
+                        <div className="input-group">
+                            <div className="custom-file">
+                                <input type="file" className="custom-file-input" aria-describedby="inputGroupFileAddon01" onChange={(e) => setFile(e.target.files[0])} id="myfile" name="myfile" />
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">{file.name}</label>
+                            </div>
+                        </div>
 
-        return <></>
+                        {seleccionaAsignatura()}
+
+                        <div className="upload-message-section">
+                            {file !== '' && idAsignatura &&
+                                <button type="button" className="btn btn-success btn-submit" onClick={uploadFile}>Subir Preguntas</button>
+                            }
+                        </div>
+                    </div>
+
+                </div>
+                <ErrorModal id={"inserted_error"} message={message}></ErrorModal>
+                <SuccessModal id={"inserted_success"} message={message}></SuccessModal>
+            </div>
+        )
     }
 }
+
+    
+export default UploadQuestions;
