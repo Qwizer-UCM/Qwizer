20c0e3e28b19ca085f9e127060fbe28c051e389e
Tiberiuss <vroman01@ucm.es>
2022-10-11 00:40:02 +0200

    Arreglo añadir alumno

--- qwizer_fe/src/components/RegisterContainer.js
+++ qwizer_fe/src/components/RegisterContainer.js
@@ -1,86 +1,71 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import DataTable from "react-data-table-component";
 import ErrorModal from "./common/modals/ErrorModal";
 import SuccessModal from "./common/modals/SuccessModal";
 import Users from "../services/Users";
 import Subjects from "../services/Subjects";
 
+
+const columns = [
+  {
+    name: "Id",
+    selector: (row) => row.id,
+    sortable: true,
+    omit: true,
+  },
+  {
+    name: "Nombre",
+    selector: (row) => row.nombre,
+    sortable: true,
+  },
+  {
+    name: "Apellidos",
+    selector: (row) => row.apellidos,
+    sortable: true,
+  },
+];
+
 const RegisterContainer = (props) => {
   const [asignaturas, setAsignaturas] = useState([]);
-  const [alumnos, setAlumnos] = useState([]);
-  const [columns, setColumns] = useState(undefined);
   const [data, setData] = useState(undefined);
-  const [title, setTitle] = useState(undefined);
   const [alumnosSeleccionados, setAlumnosSeleccionados] = useState(undefined);
   const [message, setMessage] = useState(undefined);
+  const selectedStudent = useRef();
 
   useEffect(() => {
     getAsignaturas();
     getAlumnos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    generar_tabla();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [alumnos]);
-
-  useEffect(() => {
-    asignaturas.forEach((asignatura, indx) => {
-      window.$("#subject-selector").append(new Option(asignatura.nombre, asignatura.id));
-    });
-  }, [asignaturas]);
-
+    
   const getAsignaturas = () => {
     Subjects.getFromStudentOrTeacher().then(({ data }) => {
-      setAsignaturas(data.asignaturas);
+        setAsignaturas(data.asignaturas);
     });
   };
 
   const getAlumnos = () => {
     Users.getStudents().then(({ data }) => {
-      setAlumnos(data.alumnos);
-    });
-  };
 
-  const generar_tabla = () => {
-    let columns = [
-      {
-        name: "Id",
-        selector: (row) => row.id,
-        sortable: true,
-        omit: true,
-      },
-      {
-        name: "Nombre",
-        selector: (row) => row.nombre,
-        sortable: true,
-      },
-      {
-        name: "Apellidos",
-        selector: (row) => row.apellidos,
-        sortable: true,
-      },
-    ];
-    let data = [];
-    alumnos.forEach((alumno, indx) => {
-      let row = {
-        id: alumno.id,
-        nombre: alumno.nombre,
-        apellidos: alumno.apellidos,
-      };
-      data.push(row);
+      let students = [];
+      data.alumnos.forEach((alumno, indx) => {
+        let row = {
+          id: alumno.id,
+          nombre: alumno.nombre,
+          apellidos: alumno.apellidos,
+        };
+        students.push(row);
+      });
+  
+      setData(students);
     });
-
-    setColumns(columns);
-    setData(data);
-    setTitle("Alumnos matriculados en el centro");
   };
 
   const registrarAlumnos = () => {
     let alumnos = alumnosSeleccionados;
 
-    let asignatura = window.$("#subject-selector").val();
+
+    let asignatura = selectedStudent.current.options[selectedStudent.current.selectedIndex].value;
 
     if (asignatura === "Selecciona una asignatura" || alumnos === undefined || alumnos.length === 0) {
       setMessage("Selecciona una asignatura y al menos un alumno");
@@ -109,19 +94,24 @@ const RegisterContainer = (props) => {
     setAlumnosSeleccionados(selectedRows);
   };
 
+  if(!data) return null;
+
   return (
     <div className="index-body">
       <div className="card tabla-notas">
         <div className="card-content">
           <h4 className="d-flex justify-content-center">Registro de alumnos en asignaturas</h4>
           <label>Selecciona la asignatura a la que quieras añadir a los alumnos</label>
-          <select className="form-select" id="subject-selector" aria-label="Default select example">
+          <select className="form-select" id="subject-selector" aria-label="Default select example" ref={selectedStudent}>
             <option hidden defaultValue>
               Selecciona una asignatura
             </option>
+            {asignaturas.map((asignatura, idx) => (
+              <option key={asignatura.id} value={asignatura.id}>{asignatura.nombre}</option>
+            ))}
           </select>
           <br />
-          <DataTable pointerOnHover selectableRows pagination theme={"default"} title={title} columns={columns} data={data} onSelectedRowsChange={handleChange}></DataTable>
+          <DataTable pointerOnHover selectableRows pagination theme={"default"} title={"Alumnos matriculados en el centro"} columns={columns} data={data} onSelectedRowsChange={handleChange}></DataTable>
           <div className="d-flex justify-content-center">
             <button className="btn btn-primary" onClick={registrarAlumnos}>
               Registrar alumnos
