3c9fb0966e5dccba0c7f8b6e4fba69459e8c7840
Tiberiuss <vroman01@ucm.es>
2022-10-12 16:40:55 +0200

    Gestión correcta del token del usuario

--- qwizer_be/api/urls.py
+++ qwizer_be/api/urls.py
@@ -1,50 +1,53 @@
-from django.urls import path,include
+from django.urls import path
 from rest_framework import routers
+from djoser.views import UserViewSet, TokenCreateView, TokenDestroyView
 from .viewss import TestsViewSet
 from . import views
 
 router = routers.DefaultRouter()
 
-router.register('tests',TestsViewSet,'tests')
+router.register("tests", TestsViewSet, "tests")
 
 urlpatterns = router.urls
 urlpatterns += [
-    #TODO Esto es un paquete que ofrece metodos para autenticar
+    # TODO Esto es un paquete que ofrece metodos para autenticar
     # revisar si nos renta o es mejor implementarlo a mano
     # https://djoser.readthedocs.io/en/latest/getting_started.html
-    path("auth/", include("djoser.urls")),
+    path("auth/user/me", UserViewSet.as_view({"get": "me"})),
+    path("auth/token/login", TokenCreateView.as_view()),
+    path("auth/token/logout", TokenDestroyView.as_view()),
 
-    #TODO No he visto que se use
-    path('register',views.registro,name='register'),
+    # TODO No he visto que se use
+    path("register", views.registro, name="register"),
 
-    #NoSeDondeMeterloTodavia
-    path('insert-qr', views.insert_qr, name='insert-qr'),
-    path('get-hashes', views.get_hashes, name='get-hashes'),
+    # NoSeDondeMeterloTodavia
+    path("insert-qr", views.insert_qr, name="insert-qr"),
+    path("get-hashes", views.get_hashes, name="get-hashes"),
 
-    #Questions
-    path('delete-question', views.delete_question, name='delete-question'),
-    path('update-question', views.update_question, name='update-question'),
-    path('upload-questions', views.upload_questions, name='upload-questions'),
+    # Questions
+    path("delete-question", views.delete_question, name="delete-question"),
+    path("update-question", views.update_question, name="update-question"),
+    path("upload-questions", views.upload_questions, name="upload-questions"),
 
-    #Subjects
-    path('get-subjects',views.get_subjects,name='get-subjects'),
-    path('get-quizzes', views.get_quizzes, name='get-quizzes'),
-    path('get-all-subjects',views.get_all_subjects,name='get-all-subjects'),
-    path('get-subject-questions', views.get_subject_questions, name='get-subject-questions'),
-    path('enroll-students', views.enroll_students, name='enroll-students'),
-    path('get-subject-info', views.get_subject_info, name='get-subject-info'),
+    # Subjects
+    path("get-subjects", views.get_subjects, name="get-subjects"),
+    path("get-quizzes", views.get_quizzes, name="get-quizzes"),
+    path("get-all-subjects", views.get_all_subjects, name="get-all-subjects"),
+    path("get-subject-questions",views.get_subject_questions, name="get-subject-questions"),
+    path("enroll-students", views.enroll_students, name="enroll-students"),
+    path("get-subject-info", views.get_subject_info, name="get-subject-info"),
 
-    #Tests
-    path('response',views.response,name='response'),
-    path('test-corrected',views.testCorrected,name='test-corrected'),
-    path('create-quiz', views.create_quiz, name='create-quiz'),
-    path('get-quiz-grades', views.get_quiz_grades, name='get-quiz-grades'),
-    path('test',views.test,name='test'),
-    path('get-quiz-info', views.get_quiz_info, name='get-quiz-info'),
-    path('upload', views.upload, name='upload'),
-
-    #Users
-    path('login',views.app_login,name='login'),
-    path('logout',views.app_logout,name='logout'),
-    path('get-students', views.get_students, name='get-students'),
+    # Tests
+    path("response", views.response, name="response"),
+    path("test-corrected", views.testCorrected, name="test-corrected"),
+    path("create-quiz", views.create_quiz, name="create-quiz"),
+    path("get-quiz-grades", views.get_quiz_grades, name="get-quiz-grades"),
+    path("test", views.test, name="test"),
+    path("get-quiz-info", views.get_quiz_info, name="get-quiz-info"),
+    path("upload", views.upload, name="upload"),
+  
+    # Users
+    path("login", views.app_login, name="login"),
+    path("logout", views.app_logout, name="logout"),
+    path("get-students", views.get_students, name="get-students"),
 ]
--- qwizer_fe/src/services/Users.js
+++ qwizer_fe/src/services/Users.js
@@ -1,9 +1,9 @@
 import client from '../client';
 
 const Users = {
-  //TODO revisar funcion de abajo; y comprobar si pasa algo por mandar auth undefined 
-  login: (email, password) => client.post('login',{email, password}),
-  logout: () => client.get('logout'),
+  me: () => client.get('auth/user/me'),
+  login: (email, password) => client.post('auth/token/login',{email, password}),
+  logout: () => client.post('auth/token/logout'),
   getStudents: () => client.post('get-students'),
 }
 
